33aabc15c01912199b8a502f8034772f2201ff98
unknown <miguelsant2001@gmail.com>
2022-11-17 17:16:02 -0500

    pe

--- network/routes.js
+++ network/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const message = require('../components/message/network');
 
 const routes = function (server) {
-  server.use('/main', message);
+  server.use('/', message);
 };
 
 module.exports = routes;
